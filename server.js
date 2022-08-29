bdc743dd03a9329cd6bfb87823b352fb8eb98ca2
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-08-29 14:00:44 -0400

    checked routes

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const methodOverride  = require('method-override');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config()
 const Travels = require('./models/travels.js');
 const cors = require('cors');
+const db = mongoose.connection
 
 //middleware//
 app.use(cors());
 app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 const PORT = process.env.PORT || 3003;
 const MONGODB_URI  = process.env.MONGODB_URI 
-const db = mongoose.connection
 
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
@@ -23,6 +27,9 @@ app.post('/travels', (req, res) => {
         res.json(createdTravel);
     });
 });
+app.get('/' , (req, res) => {
+    res.send('Hello World!');
+  });
 //index route
 app.get('/travels', (req, res) => {
     Travels.find({}, (err, foundTravels) => {
@@ -42,9 +49,7 @@ app.put('/travels/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('listening...')
-})
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 mongoose.connect(MONGODB_URI, () => {
     console.log('whatever')
 })
